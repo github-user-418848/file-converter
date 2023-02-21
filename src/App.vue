0064a2109ad46c52c9eb0884097fc6192e5f2e34
github-user-418848 <x1cjlyjr@duck.com>
2023-02-21 16:19:58 +0800

    changed navigation to select option

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
+<script>
+  export default {
+    methods: {
+      changeRoute(e) {
+        this.$router.push("/" + e.target.value);
+      },
+    },
+  }
+</script>
+
 <template>
   <div class="content">
     <h1>Convert to DAT File</h1>
-    <div class="nav-links">
+    <!-- <div class="nav-links">
       <router-link to="/">MAP</router-link>
       <router-link to="/qap">QAP</router-link>
-    </div>
+    </div> -->
+    <select name="route" id="route" v-on:change="changeRoute($event)">
+      <option value="" selected>MAP</option>
+      <option value="qap">QAP</option>
+    </select>
     <router-view v-slot="{ Component }">
       <transition name="fade" mode="out-in">
         <component :is="Component"/>
@@ -38,5 +52,13 @@
   .fade-enter-active,
   .fade-leave-active {
     transition: opacity 0.5s ease-in-out;
+  }
+
+  select {
+    border: 0;
+    outline: none;
+    background: rgba(0, 0, 0, .123);
+    width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+    height: 35px;
   }
 </style>
