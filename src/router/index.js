cc15c539a59a6045c1593ed8db9d1e072c1351b3
github-user-418848 <x1cjlyjr@duck.com>
2023-02-28 13:01:23 +0800

    changed routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,33 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import DataMapping from '../components/DataMapping.vue'
+
+import MapReport from '../views/MapReport.vue'
+import QapReport from '../views/QapReport.vue'
+import SawtReport from '../views/SawtReport.vue'
+import SalesReport from '../views/SalesReport.vue'
+import PurchaseReport from '../views/PurchaseReport.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
-            component: DataMapping
+            component: MapReport,
+        },
+        {
+            path: '/qap-report',
+            component: QapReport,
+        },
+        {
+            path: '/sawt-report',
+            component: SawtReport,
+        },
+        {
+            path: '/sales-report',
+            component: SalesReport,
+        },
+        {
+            path: '/purchase-report',
+            component: PurchaseReport,
         },
     ]
 })
