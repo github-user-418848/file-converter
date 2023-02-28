eae76d6b940095ba9722f8333d50d007aeb55781
github-user-418848 <x1cjlyjr@duck.com>
2023-02-28 13:13:37 +0800

    created a card component

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card" v-if="fileName">
+        <span class="file-info">{{ fileName }}</span>
+        <button class="btn" @click="downloadFile()">Download</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            fileName: String,
+            downloadFile: Function,
+        },
+    }
+</script>
+
+<style>
+    .card {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        padding: 1rem;
+        width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+        margin: 0 auto;
+        justify-content: space-between;
+        border-radius: 20px;
+        background: rgba(0, 0, 0, 0.123);
+    }
+</style>
