0079eb2fb31ab4cca6dc7697614c6f41d3ddc5c1
github-user-418848 <x1cjlyjr@duck.com>
2023-02-28 13:32:25 +0800

    add dropzone and card component

--- src/views/MapReport.vue
+++ src/views/MapReport.vue
@@ -1,3 +1,24 @@
 <template>
-    
+
+    <Card fileName="true" :downloadFile="download"/>
 </template>
+
+<script>
+
+    import Card from '../components/Card.vue';
+    import DropZone from '../components/DropZone.vue';
+
+    export default {
+        name: "MapReport",
+        components: {
+            Card,
+            DropZone,
+        },
+        methods: {
+            download() {
+                alert("Downloaded");
+            }
+        }
+    }
+
+</script>
