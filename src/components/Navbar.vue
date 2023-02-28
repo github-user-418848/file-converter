30d352251c8b842ac7fd7c2ebb8a63e400583775
github-user-418848 <x1cjlyjr@duck.com>
2023-02-28 14:53:22 +0800

    add file together with the routing links

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <router-link to="/">Map Report</router-link>
+      <router-link to="/qap-report">QAP Report</router-link>
+      <router-link to="/sales-report">SAWT Report</router-link>
+      <router-link to="/sales-report">Sales Report</router-link>
+      <router-link to="/sales-report">Purchase Report</router-link>
+    </div>
+</template>  
