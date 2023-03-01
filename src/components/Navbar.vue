7fbbfd5fa44f6dd4827033d6ad2336625bd94450
github-user-418848 <x1cjlyjr@duck.com>
2023-03-01 13:35:55 +0800

    created dynamic router links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,62 @@
 <template>
-    <div>
-      <router-link to="/">Map Report</router-link>
-      <router-link to="/qap-report">QAP Report</router-link>
-      <router-link to="/sales-report">SAWT Report</router-link>
-      <router-link to="/sales-report">Sales Report</router-link>
-      <router-link to="/sales-report">Purchase Report</router-link>
-    </div>
+  Select a filetype to convert with:
+  <nav>
+    <ul>
+      <li v-for="report in reports" :key="report.id">
+        <router-link :to="{ name: 'GenReport', params: { id: report.id } }">
+          {{ report.title }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
+<script>
+  export default {
+    name: "Navbar",
+    data() {
+      return {
+        reports: [
+          { title: 'MAP', id: 1 },
+          { title: 'QAP', id: 2 },
+          { title: 'SAWT', id: 3 },
+          { title: 'Sales', id: 4 },
+          { title: 'Purchase', id: 5 },
+        ]
+      }
+    }
+  }
+</script>
+<style>
+  nav {
+    max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+    width: 100%;
+    margin: 0 auto;
+    overflow-x: auto;
+    padding: .5rem;
+    border: 1px solid lightgray;
+  }
+  nav ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: .5rem;
+    padding: 0px;
+  }
+  nav ul li {
+    list-style-type: none;
+  }
+  nav ul li a {
+    padding: clamp(0.2rem, 0.2rem + 0.25vw, 0.5rem);
+    border-radius: 8px;
+    transition: 0.3s;
+  }
+  nav ul li a:hover {
+    
+    background-color: var(--primary);
+    color: white;
+  }
+  nav ul li a.router-link-active {
+    background-color: var(--primary);
+    color: white;
+  }
+</style>
