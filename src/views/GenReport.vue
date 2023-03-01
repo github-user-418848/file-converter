7b3bc34de15b6d8021084238a9504bc7a4dc84a0
github-user-418848 <x1cjlyjr@duck.com>
2023-03-01 13:38:25 +0800

    added a GenReport.vue

---
+++ src/views/GenReport.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- {{ $route.params.id }} -->
+    <DropZone @drop.prevent="drop" @change="selectedFile"/>
+    <Card fileName="true" :downloadFile="download"/>
+</template>
+
+<script>
+
+    import Card from '../components/Card.vue';
+    import DropZone from '../components/DropZone.vue';
+
+    export default {
+        name: "GenReport",
+        components: {
+            Card,
+            DropZone,
+        },
+        methods: {
+            download() {
+                alert("Downloaded");
+            }
+        }
+    }
+
+</script>
