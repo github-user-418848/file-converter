30a417f78ce0e22b941db9f41147cad51b22b492
github-user-418848 <x1cjlyjr@duck.com>
2023-03-01 13:34:36 +0800

    changed download card and button style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -20,11 +20,24 @@
         display: flex;
         flex-wrap: wrap;
         align-items: center;
-        padding: 1rem;
-        width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+        padding: .8rem;
+        max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+        width: 100%;
         margin: 0 auto;
         justify-content: space-between;
-        border-radius: 20px;
-        background: rgba(0, 0, 0, 0.123);
+        border: 1.5px solid lightgray;
+    }
+    
+    .btn {
+        padding: 8px 12px;
+        border-radius: 8px;
+        color: white;
+        background-color: var(--primary);
+        transition: .3s ease all;
+        cursor: pointer;
+    }
+    .btn:hover {
+        background-color: white;
+        color: var(--primary);
     }
 </style>
