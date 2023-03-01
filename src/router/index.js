0c5c4dec45ec6a5c5649bef0bac50f8a2addd1c8
github-user-418848 <x1cjlyjr@duck.com>
2023-03-01 13:37:52 +0800

    del & rm routes and changed to gen-dat-file w/ GET

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import MapReport from '../views/MapReport.vue'
-import QapReport from '../views/QapReport.vue'
-import SawtReport from '../views/SawtReport.vue'
-import SalesReport from '../views/SalesReport.vue'
-import PurchaseReport from '../views/PurchaseReport.vue'
+import GenReport from '../views/GenReport.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        // {
+        //     path: '/gen-dat-file',
+        //     component: GenReport,
+        // },
         {
-            path: '/',
-            component: MapReport,
-        },
-        {
-            path: '/qap-report',
-            component: QapReport,
-        },
-        {
-            path: '/sawt-report',
-            component: SawtReport,
-        },
-        {
-            path: '/sales-report',
-            component: SalesReport,
-        },
-        {
-            path: '/purchase-report',
-            component: PurchaseReport,
+            name: 'GenReport',
+            path: '/gen-dat-file/:id',
+            component: GenReport,
         },
     ]
 })
