008fddbc17e56d167ab20e390da21b45affa9b9c
github-user-418848 <x1cjlyjr@duck.com>
2023-03-03 14:26:08 +0800

    added readXMLFile Func

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,17 @@
-// Extract XML
-// Upload Files
-// 
+export function readXMLFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(reader.result, 'text/xml');
+            resolve(xmlDoc);
+        };
+
+        reader.onerror = () => {
+            reject(reader.error);
+        };
+
+        reader.readAsText(file);
+    });
+}
