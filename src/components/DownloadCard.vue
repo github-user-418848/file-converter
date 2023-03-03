0f985b6ca06d25e10c8df4f2acc86fb6754c3fad
github-user-418848 <x1cjlyjr@duck.com>
2023-03-03 14:25:35 +0800

    add a DLFunction, commented out for future changes

--- src/components/DownloadCard.vue
+++ src/components/DownloadCard.vue
@@ -4,65 +4,85 @@
             <span class="file-info">{{ fileName }}</span>
         </div>
         <div class="col-2">
-            <input type="text" name="rdo_code" id="rdo_code" placeholder="Enter RDO Code">
+            <input type="text" name="rdo_code" id="rdo_code" placeholder="RDO Code:">
             <button class="btn" @click="downloadFile()">Download</button>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Card",
-        props: {
-            fileName: String,
-            downloadFile: Function,
-        },
-    }
+
+export default {
+    name: "Card",
+    props: {
+        fileName: String,
+        downloadFile: Function,
+    },
+    // methods: {
+    //     downloadFile() {
+    //         const link = document.createElement('a');
+    //         const file = new Blob([MAPContent.value], { type: 'text/plain' });
+    //         link.href = URL.createObjectURL(file);
+    //         const dateNow = convertToReadableDateFormat(new Date());
+    //         link.download = 'maprpt' + dateNow + '.dat';
+    //         link.click();
+    //         URL.revokeObjectURL(link.href);
+    //     }
+    // }
+}
+
 </script>
 
 <style>
-    .btn {
-        padding: 8px 12px;
-        border-radius: 0px;
-        background-color: white;
-        border: 1.5px solid var(--primary);
-        color: var(--primary);
-        transition: .3s ease all;
-        cursor: pointer;
+.btn {
+    padding: 8px 12px;
+    border-radius: 0px;
+    background-color: white;
+    border: 1.5px solid var(--primary);
+    color: var(--primary);
+    transition: .3s ease all;
+    cursor: pointer;
+}
+
+.btn:hover {
+    color: white;
+    background-color: var(--primary);
+}
+
+.card {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: .8rem;
+    max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+    width: 100%;
+    margin: 0 auto;
+    justify-content: space-between;
+    border: 1.5px solid lightgray;
+}
+
+.col-1,
+.col-2 {
+    width: 100%;
+}
+
+input {
+    background-color: white;
+    color: black;
+    outline: none;
+    border: 1px solid lightgray;
+    padding: 10px;
+}
+
+@media screen and (min-width: 920px) {
+    .col-1 {
+        width: 25%;
+        text-align: left;
     }
-    .btn:hover {
-        color: white;
-        background-color: var(--primary);
+
+    .col-2 {
+        width: 75%;
+        text-align: right;
     }
-    .card {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        padding: .8rem;
-        max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
-        width: 100%;
-        margin: 0 auto;
-        justify-content: space-between;
-        border: 1.5px solid lightgray;
-    }
-    .col-1, .col-2 {
-        width: 100%;
-    }
-    input {
-        background-color: white;
-        color: black;
-        outline: none;
-        border: 1px solid lightgray;
-        padding: 10px;
-    }
-    @media screen and (min-width: 920px) {
-        .col-1 {
-            width: 25%;
-            text-align: left;
-        }
-        .col-2 {
-            width: 75%;
-            text-align: right;
-        }
-    }
+}
 </style>
