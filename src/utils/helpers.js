991ca05c3d3faa472fc02cdb1287b3999e8dbb0d
github-user-418848 <x1cjlyjr@duck.com>
2023-03-06 15:27:58 +0800

    added a func condition for each report

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15,3 +15,43 @@ export function readXMLFile(file) {
         reader.readAsText(file);
     });
 }
+
+export function reportTypeFieldName(route) {
+
+    let fieldNames = []
+
+    switch (route) {
+        case '1':
+            fieldNames = [
+                "RecordNumber2", "tin2", "wtCode", "WTName2",
+                "Rate2", "vendname3", "Sumoftaxamt2", "Sumoftaxableamt2"
+            ];
+            break;
+        case '2':
+            fieldNames = [
+                "Sumoftaxableamt2", "SumofRate1", "Sumoftaxamt2",
+                "vendname3", "wtcode2", "WTName2", "tin2", "GroupNumber1"
+            ];
+            break;
+        case '3':
+            fieldNames = [
+                "tin2", "wtcode2", "WTName2",
+                "Rate2", "vendname3", "Sumoftaxamt2", "Sumoftaxableamt2", "GroupNumber1"
+            ];
+            break;
+        case '4':
+            fieldNames = [
+                "AmountOfPurchaseOfCapitalGoods1", "AmountOfPurchaseOfServices1", "AmountOfTaxablePurchase1",
+                "AmountOfZeroRatedPurchase1", "AmountOfExemptPurchase1", "AmountOfGrossPurchase1", "cardname2", "TIN2",
+                "VAT1", "docdate1", "Total2"
+            ];
+            break;
+        case '5':
+            fieldNames = [
+                "Text1", "TIN2", "VAT1", "docdate1", "Text15", "Text16", "Text17", "Text18", "Text19",
+                "Text20", "Text3", "Text9", "Total3"
+            ]
+    }
+
+    return fieldNames
+}
