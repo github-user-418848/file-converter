65134171f03cf4c74e0ec5ef88e0235a9a38f0d5
github-user-418848 <x1cjlyjr@duck.com>
2023-03-07 14:33:25 +0800

    added RdoInputCard component

---
+++ src/components/RdoInputCard.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card">
+        <label for="rdo_code">RDO Code: </label>
+        <input type="text" name="rdo_code" id="rdo_code" placeholder="RDO Code:">
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "RdoInputCard",
+}
+
+</script>
+
+<style>
+.card {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: .8rem;
+    max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+    width: 100%;
+    margin: 0 auto;
+    justify-content: space-between;
+    border: 1.5px solid lightgray;
+}
+
+input {
+    background-color: white;
+    color: black;
+    outline: none;
+    border: 1px solid lightgray;
+    padding: 10px;
+}
+</style>
