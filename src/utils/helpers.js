ad635a54d1aa8555c750b48ee04d3ee5bb962b10
github-user-418848 <x1cjlyjr@duck.com>
2023-03-08 12:32:56 +0800

    added createTextDataHeader & fomatTIN func

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -15,3 +15,24 @@ export function readXMLFile(file) {
         reader.readAsText(file);
     });
 }
+
+export function createTextDataHeader(record, route) {
+    let textDataHeader = ''
+    switch (route) {
+        case '1':
+            textDataHeader += `HMAP,H1600VT,${formatTIN(record[0][0])},${record[0][1]},${record[0][2]}`
+            break;
+    }
+    return textDataHeader;
+}
+
+function formatTIN(string) {
+    const tinRegex = /TIN:\s*(\d{3})-(\d{3})-(\d{3})-(\d{4})/;
+    const tinMatch = string.match(tinRegex);
+    let outputString = ''
+    
+    if (tinMatch) {
+        outputString = tinMatch[1] + tinMatch[2] + tinMatch[3] + ',' + tinMatch[4];
+    }
+    return outputString
+}
