0912d24d7310a920973c7a370862310f537a24ad
github-user-418848 <x1cjlyjr@duck.com>
2023-03-13 10:37:32 +0800

    added a const data for file validation

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -1,7 +1,6 @@
 import { ref } from 'vue'
 
-export const fileData = {
-    originalFileName: ref([]),
-    textContent: ref([]),
-    generatedFileName: ref([]),
-}
+export const
+fileData = {originalFileName: ref([]), textContent: ref([]), generatedFileName: ref([]),},
+allowedExtensions = ['xml',],
+sizeLimit = 10_000_000
