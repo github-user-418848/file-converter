b164d49dd995ee92d784a8f9eafa5612b61c40e0
github-user-418848 <x1cjlyjr@duck.com>
2023-03-13 11:11:21 +0800

    improved toast UI

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="toast-wrapper">
         <div class="toast">
-        An error occurred
+            An error occurred
         </div>
     </div>
 </template>
 
 <style>
-    .toast-wrapper {
-        position: fixed;
-        width: 100%;
-        top: 20px;
-    }
-    .toast {
-        padding: 20px;
-        color: white;
-        background: #ff0062;
-        border-radius: 10px;
-        box-shadow: 1px 3px 5px rgba(0,0,0,0.2);
-        max-width: 400px;
-        margin: 0 auto;
-    }
+.toast-wrapper {
+	position: fixed;
+	max-width: clamp(9.375rem, 9.375rem + 23.4375vw, 37.5rem);
+	top: 20px;
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	margin: 0 auto;
+	left: calc(clamp(0rem, 0rem + 100vw, 120rem) / 2 - clamp(9.375rem, 9.375rem + 23.4375vw, 37.5rem) / 2);
+}
+.toast {
+    padding: clamp(0.625rem, 0.625rem + 0.5208vw, 1.25rem);
+    color: white;
+    background: #ff0062;
+    border-radius: 10px;
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.2);
+    max-width: 100%;
+    width: 100%;
+    margin: 0 auto;
+}
 </style>
