3fd47177192c9074734cf12b8c6488f50da439d3
github-user-418848 <x1cjlyjr@duck.com>
2023-03-13 10:37:50 +0800

    created validators for xml file

---
+++ src/utils/validators.js
@@ -0,0 +1,13 @@
+import { allowedExtensions, sizeLimit } from "./globals.js";
+
+export async function validateXmlFile(file) {
+    if (!allowedExtensions.includes(file.name.split(".").pop())) {
+        throw "Filetype should be only .xml"
+    }
+    else if (file) {
+        throw "Unable to load file"
+    }
+    else if (file.size > sizeLimit) {
+        throw `File should not exceed to ${sizeLimit/1_000_000}MB`
+    }
+}
