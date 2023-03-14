f4e1f8c251210babd35ddea0aea3961d45487c65
github-user-418848 <x1cjlyjr@duck.com>
2023-03-14 17:40:43 +0800

    removed

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -4,9 +4,6 @@ export async function validateXmlFile(file) {
     if (!allowedExtensions.includes(file.name.split(".").pop())) {
         throw "Filetype should be only .xml"
     }
-    // else if (file) {
-    //     throw "Unable to load file"
-    // }
     else if (file.size > sizeLimit) {
         throw `File should not exceed to ${sizeLimit/1_000_000}MB`
     }
