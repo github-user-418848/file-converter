f972c79fae40b9a10b2ec9e445de78f0595a11d0
github-user-418848 <x1cjlyjr@duck.com>
2023-03-22 13:37:10 +0800

    increased sizeLimit to 35mb

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -3,4 +3,4 @@ import { ref } from 'vue'
 export const
 fileData = {originalFileName: ref([]), textContent: ref([]), generatedFileName: ref([]),},
 allowedExtensions = ['xml',],
-sizeLimit = 10_000_000
+sizeLimit = 35_000_000
