43b5252530fd50d61ebc3f6934b567a835ebcefc
github-user-418848 <x1cjlyjr@duck.com>
2023-03-23 15:55:17 +0800

    added form type select input

--- src/components/RdoInputCard.vue
+++ src/components/RdoInputCard.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="card">
-        <label for="rdo_code">RDO Code: </label>
+        <label for="form_type">Form Type</label>
+        <select name="report_type" id="report_type">
+            <option v-for="report in filteredReports" :key="report.id" :value="report.id" :selected="isFirstMatchingReport(report)">
+                {{ report.name }}
+            </option>
+        </select>
+    </div>
+    <div class="card">
+        <label for="rdo_code">RDO Code </label>
         <input type="text" name="rdo_code" id="rdo_code">
     </div>
 </template>
@@ -9,6 +17,36 @@
 
 export default {
     name: "RdoInputCard",
+    data() {
+        return {
+            reports: [
+                { id: 1, index: 1, name: '1601e' },
+                { id: 2, index: 1, name: '1601f' },
+                { id: 3, index: 1, name: '1600' },
+
+                { id: 1, index: 3, name: '1700' },
+                { id: 2, index: 3, name: '1701' },
+                { id: 3, index: 3, name: '1702q' },
+                { id: 4, index: 3, name: '1702' },
+                { id: 5, index: 3, name: '2550m' },
+                { id: 6, index: 3, name: '2550q' },
+                { id: 7, index: 3, name: '2551m' },
+                { id: 8, index: 3, name: '2553' },
+            ]
+        }
+    },
+
+    computed: {
+        filteredReports() {
+            return this.reports.filter(report => report.index === parseInt(this.$route.params.id));
+        },
+    },
+
+    methods: {
+        isFirstMatchingReport(report) {
+            return this.filteredReports.indexOf(report) === 0;
+        },
+    }
 }
 
 </script>
@@ -23,15 +61,24 @@ export default {
     width: 100%;
     margin: 0 auto;
     justify-content: space-between;
-    border: 1.5px solid lightgray;
+    /* border: 1.5px solid lightgray; */
+    background-color: #e4e4e4;
+    color: rgb(90, 94, 99);
 }
 
-input {
+input,
+select {
     background-color: white;
     color: black;
     outline: none;
     border: 1px solid rgb(124, 124, 124);
+    border-radius: 8px;
     padding: clamp(0.3125rem, 0.3125rem + 0.2604vw, 0.625rem);
-    width: 65%;
+    width: 100%;
+    max-width: 270px;
+}
+
+select {
+    max-width: 288px;
 }
 </style>
