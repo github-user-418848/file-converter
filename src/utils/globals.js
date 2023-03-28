ce98d7b846ad06f9c09ab000f701cb3ed36452b0
github-user-418848 <x1cjlyjr@duck.com>
2023-03-28 18:16:07 +0800

    added quarterly alphalist of payees option

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -2,13 +2,14 @@ import { ref } from 'vue'
 
 export const
 
-    files = {originalFileName: ref([]), textContent: ref([]), generatedFileName: ref([]),},
+    files = { originalFileName: ref([]), textContent: ref([]), generatedFileName: ref([]), },
     allowedExtensions = ['xml',],
     sizeLimit = 35_000_000,
     minRdoCodeLength = 0,
     maxRdoCodeLength = 5,
     reportTypes = [
         { id: 'map', title: 'MAP', name: 'Monthly Alphalist of Payees' },
+        { id: 'qap', title: 'QAP', name: 'Quarterly Alphalist of Payees' },
         { id: 'sawt', title: 'SAWT', name: 'Summary Alphalist of Withholding Taxes' },
     ],
     formTypes = [
@@ -16,6 +17,11 @@ export const
         { id: 2, index: 'map', name: '1601F' },
         { id: 3, index: 'map', name: '1600VT' },
         { id: 4, index: 'map', name: '1600PT' },
+
+        { id: 1, index: 'qap', name: '1601EQ' },
+        { id: 2, index: 'qap', name: '1601FQ' },
+        { id: 3, index: 'qap', name: '1621' },
+
         { id: 1, index: 'sawt', name: '1700' },
         { id: 2, index: 'sawt', name: '1701' },
         { id: 3, index: 'sawt', name: '1702Q' },
