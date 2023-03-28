ec127db574b01dd4c45ec389866c583424796d3b
github-user-418848 <x1cjlyjr@duck.com>
2023-03-28 17:09:39 +0800

    minor style changes

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,9 +1,25 @@
 <template>
-    <h1>Not Found</h1>
-    <a href="/">Return to main page</a>
+    <div class="not-found">
+        <h2>Not Found</h2>
+        <a href="/">Return to main page</a>
+    </div>
 </template>
 <script>
     export default {
         name: 'NotFound',
     }
 </script>
+<style>
+
+.not-found {
+    min-height: 50vh;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    border: 1px solid lightgray;
+    margin: 1rem auto .5rem;
+    max-width: clamp(12.5rem, 12.5rem + 26.0417vw, 43.75rem);
+}
+
+</style>
