cc1791d6a65a750923038f26996a2cdd89dc06b8
github-user-418848 <x1cjlyjr@duck.com>
2023-03-28 14:51:21 +0800

    updated routing code with additional checks

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { allowedFormTypes, allowedReportTypes } from '../utils/globals.js'
 
+import NotFound from '../views/NotFound.vue'
 import GenReport from '../views/GenReport.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        // {
-        //     path: '/gen-dat-file',
-        //     component: GenReport,
-        // },
         {
+            name: 'Root',
             path: '/',
-            redirect: '/gen-dat-file/1'
+            redirect: '/gen-dat-file/map/1601E'
         },
         {
             name: 'GenReport',
-            path: '/gen-dat-file/:id',
+            path: '/gen-dat-file/:report_type/:form_type',
             component: GenReport,
+            beforeEnter: (to, from, next) => {
+
+                const reportType = to.params.report_type
+                const formType = to.params.form_type
+
+                if (!allowedReportTypes.includes(reportType) || !allowedFormTypes.includes(formType)) {
+                    next({ name: 'NotFound' })
+                }
+                else if (!reportType || !formType) {
+                    next({ name: 'NotFound' })
+                }
+                else {
+                    next()
+                }
+            }
         },
+        {
+            name: 'NotFound',
+            path: '/not-found',
+            component: NotFound
+        }
     ]
 })
 
