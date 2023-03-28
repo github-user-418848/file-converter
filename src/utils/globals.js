40a8be6b676f7154cc2c612d4c4e570030ca54d4
github-user-418848 <x1cjlyjr@duck.com>
2023-03-28 14:53:36 +0800

    add allowed report and form types to file upload constants

--- src/utils/globals.js
+++ src/utils/globals.js
@@ -6,4 +6,6 @@ export const
     allowedExtensions = ['xml',],
     sizeLimit = 35_000_000,
     minRdoCodeLength = 0,
-    maxRdoCodeLength = 5
+    maxRdoCodeLength = 5,
+    allowedReportTypes = ['map', 'qap', 'sawt'],
+    allowedFormTypes = ['1601E', '1601F', '1600VT', '1600PT', '1700', '1701', '1702Q', '1702', '2550M', '2550Q', '2551M', '2553']
