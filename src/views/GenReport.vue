7b349f7bd2044702c653df582cbfc4392383a78b
github-user-418848 <x1cjlyjr@duck.com>
2023-03-28 14:55:24 +0800

    updated & refactored code for better readability

--- src/views/GenReport.vue
+++ src/views/GenReport.vue
@@ -1,6 +1,7 @@
 <template>
     <Toast :errorMessage="errorMessage" />
-
+    <h1>Generate DAT File</h1>
+    <p>Convert various financial reports to DAT file</p>
     <!-- Display records array -->
     <!-- <div v-for="(record, index) in records" :key="index">
         <p>{{ index }} - {{ record }}</p>
@@ -18,6 +19,15 @@
         <DownloadCard :fileName="file" :textData="fileDataCollection.textContent[index]"
             :generatedFileName="fileDataCollection.generatedFileName[index]" />
     </div>
+    
+    <a href="https://github.com/github-user-418848/convert-to-dat-file" target="_blank">
+      <svg xmlns="http://www.w3.org/2000/svg" width="38" viewBox="0 0 24 24" xml:space="preserve">
+        <path
+          fill="var(--primary)"
+          d="M12.01.26c-6.63 0-12 5.37-12 12 0 5.3 3.44 9.8 8.21 11.39.6.11.82-.26.82-.58 0-.28-.01-1.04-.02-2.04-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.33-1.76-1.33-1.76-1.09-.74.08-.73.08-.73 1.2.08 1.84 1.24 1.84 1.24 1.07 1.83 2.81 1.3 3.49 1 .11-.78.42-1.3.76-1.6-2.66-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23.96-.27 1.98-.4 3-.4s2.05.14 3 .4c2.29-1.55 3.3-1.23 3.3-1.23.65 1.65.24 2.87.12 3.18.77.84 1.23 1.91 1.23 3.22 0 4.61-2.81 5.62-5.48 5.92.43.37.81 1.1.81 2.22 0 1.6-.01 2.9-.01 3.29 0 .32.22.69.83.58 4.76-1.59 8.2-6.08 8.2-11.38 0-6.64-5.37-12.01-12-12.01z"
+          fill-rule="evenodd" clip-rule="evenodd"></path>
+      </svg>
+    </a>
 </template>
 
 <script>
@@ -92,10 +102,10 @@ export default {
         },
 
         async createTextData(records) {
-            let textDataOutput = `${header(records, this.$route.params.id)}${details(records, this.$route.params.id)}${controls(records, this.$route.params.id)}`
+            const textDataOutput = `${header(records, this.$route.params)}${details(records, this.$route.params)}${controls(records, this.$route.params)}`
             console.log(textDataOutput)
             this.textData = textDataOutput
-            this.generatedFileName = filename(records, this.$route.params.id)
+            this.generatedFileName = filename(records, this.$route.params)
         },
 
         async pushIntoFileData() {
