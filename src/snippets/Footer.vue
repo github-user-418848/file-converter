8e76dfeec5962996f823bfb20b27c7591d73b563
github-user-418848 <x1cjlyjr@duck.com>
2023-05-11 17:08:36 +0800

    added footer info

--- src/snippets/Footer.vue
+++ src/snippets/Footer.vue
@@ -1,2 +1,18 @@
 <template>
+    <footer>
+        <p>&copy; {{ new Date().getFullYear() }} Generate DAT File</p>
+    </footer>
 </template>
+  
+<style>
+footer {
+    background-color: #e4e4e4;
+    color: #424242;
+}
+
+p {
+    padding: 1rem;
+    text-align: center;
+}
+</style>
+  
