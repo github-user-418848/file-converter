1a09d376e432d1e0ca462bc775991fc2b877244e
github-user-418848 <x1cjlyjr@duck.com>
2023-06-06 11:12:51 +0800

    refactored router configuration

--- src/router/index.js
+++ src/router/index.js
@@ -2,39 +2,65 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { reportTypes, formTypes } from '../utils/globals.js'
 
 import NotFound from '../views/NotFound.vue'
+import ValueAddedTax from '../views/ValueAddedTax.vue'
 import WithholdingTax from '../views/WithholdingTax.vue'
+
+const routes = [
+    {
+        name: 'Root',
+        path: '/',
+        redirect: '/wt/map/1601E'
+    },
+    {
+        name: 'TaxPage',
+        path: '/:tax_type/:report_type/:form_type',
+        components: {
+            default: null, // To be dynamically assigned
+        },
+        beforeEnter: (to, from, next) => {
+            const { tax_type, report_type, form_type } = to.params;
+            const report = reportTypes.find(report => report.id === report_type);
+            const form = formTypes.find(form => form.index === report_type && form.name === form_type);
+
+            if (!report || !form) {
+                next({ name: 'NotFound' });
+            } else {
+                next();
+            }
+        }
+    },
+    {
+        name: 'NotFound',
+        path: '/:pathMatch(.*)*',
+        component: NotFound
+    }
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [
-        {
-            name: 'Root',
-            path: '/',
-            redirect: '/wt/map/1601E'
-        },
-        {
-            name: 'WithholdingTax',
-            path: '/wt/:report_type/:form_type',
-            component: WithholdingTax,
-            beforeEnter: (to, from, next) => {
+    routes
+});
 
-                const reportType = to.params.report_type
-                const formType = to.params.form_type
+// Assign components dynamically based on tax_type
+router.beforeEach((to, from, next) => {
+    const { tax_type } = to.params;
+    let component = null;
 
-                if (!reportTypes.find(report => report.id === reportType) || !formTypes.find(form => form.index === reportType && form.name === formType)) {
-                    next({ name: 'NotFound' })
-                }
-                else {
-                    next()
-                }
-            }
-        },
-        {
-            name: 'NotFound',
-            path: '/:pathMatch(.*)*',
-            component: NotFound
-        }
-    ]
-})
+    if (tax_type === 'wt') {
+        component = WithholdingTax;
+    }
+    else if (tax_type === 'vat') {
+        component = ValueAddedTax;
+    }
+    else if (tax_type === 'boa') {
+        component = ValueAddedTax;
+    }
 
-export default router
+    if (component) {
+        to.matched[0].components.default = component;
+    }
+
+    next();
+});
+
+export default router;
