ff98ff06d4fdce4b75caa4fc8ed15e03acb34070
github-user-418848 <x1cjlyjr@duck.com>
2023-06-14 13:36:24 +0800

    improve readability and add support for ST reports

--- src/utils/datStructure.js
+++ src/utils/datStructure.js
@@ -1,7 +1,7 @@
-import { formatTIN, formatDate, formatAgentName, formatDigit, formatCorpName } from './formatter.js'
+import { formatTIN, formatDate, formatOwnersName, formatAgentName, formatDigit, formatCorpName } from './formatter.js'
 import { reportTypes } from './globals.js'
 
-export function header(record, route, count) {
+export function header(record, route, count = 0) {
 
     const alphaList = reportTypes.find(reportType => reportType.id === route.report_type)
     let headerData = {}
@@ -11,11 +11,11 @@ export function header(record, route, count) {
             let returnPeriodYear = ''
 
             const months = record.slice(1, -1)
-            .map(record => {
-                const [month, day, year] = record[8].split('/');
-                returnPeriodYear = `${year}`
-                return parseInt(month);
-            });
+                .map(record => {
+                    const [month, day, year] = record[8].split('/');
+                    returnPeriodYear = `${year}`
+                    return parseInt(month);
+                });
             const smallestMonth = Math.min(...months) + count
 
             headerData = {
@@ -24,7 +24,22 @@ export function header(record, route, count) {
                 registeredName: `${formatAgentName(record[0][2])}`,
                 returnPeriod: `${smallestMonth < 10 ? `0${smallestMonth}` : smallestMonth}/${returnPeriodYear}`,
             }
-        break
+            break
+        case 'st':
+            headerData = {
+                alphaListTypeCode: `H,S`,
+                tinWithBranchCode: `${formatTIN(record[0][0])}`,
+                registeredName: `${formatOwnersName(record[0][0])}`,
+                amtOfGrossSales: `${formatDigit(record[record.length - 1][0])}`,
+                amtOfExemptSales: `${formatDigit(record[record.length - 1][1])}`,
+                amtOfZeroRatedSales: `${formatDigit(record[record.length - 1][2])}`,
+                amtOfTaxableSales: `${formatDigit(record[record.length - 1][3])}`,
+                amtOfSalesOfServices: `${formatDigit(record[record.length - 1][4])}`,
+                amtOfSalesOfGoods: `${formatDigit(record[record.length - 1][5])}`,
+                amtOfOutputTax: `${formatDigit(record[record.length - 1][6])}`,
+                amtOfGrossTaxableSales: `${formatDigit(record[record.length - 1][8])}`
+            }
+            break
         default:
             headerData = {
                 alphaListTypeCode: `H${alphaList.acronym},H${route.form_type}`,
@@ -32,13 +47,13 @@ export function header(record, route, count) {
                 registeredName: `${formatAgentName(record[0][2])}`,
                 returnPeriod: `${formatDate(record[0][1])}`
             }
-        break
+            break
     }
     return headerData
 }
 
-export function details(records, route, count) {
-    
+export function details(records, route, count = 0) {
+
     const alphaList = reportTypes.find(reportType => reportType.id === route.report_type)
     let details
 
@@ -58,41 +73,58 @@ export function details(records, route, count) {
             break
         case 'qap':
             const months = records.slice(1, -1)
-            .map(record => {
-                const [month, day, year] = record[8].split('/');
-                return parseInt(month);
-            });
+                .map(record => {
+                    const [month, day, year] = record[8].split('/');
+                    return parseInt(month);
+                });
             const smallestMonth = Math.min(...months)
 
             details = records.slice(1, -1)
-            .filter(record => {
-                const [month, day, year] = record[8].split('/')
-                return month == (count + smallestMonth)
-            })
-            .map(record => ({
-                alphaListTypeCode: `D1,D${route.form_type}`,
-                incomePayment: `${formatDigit(record[0])}`,
-                taxRate: `${formatDigit(record[1])}`,
-                taxWithHeld: `${formatDigit(record[2])}`,
-                corporation: `${formatCorpName(record[3])}`,
-                atcCode: `${record[4]}`,
-                tinWithBranchCode: `${formatTIN(record[6])}`,
-                sequenceNumber: `${record[7]}`,
-                returnPeriod: `${record[8]}`,
-            }))
+                .filter(record => {
+                    const [month, day, year] = record[8].split('/')
+                    return month == (count + smallestMonth)
+                })
+                .map(record => ({
+                    alphaListTypeCode: `D1,D${route.form_type}`,
+                    incomePayment: `${formatDigit(record[0])}`,
+                    taxRate: `${formatDigit(record[1])}`,
+                    taxWithHeld: `${formatDigit(record[2])}`,
+                    corporation: `${formatCorpName(record[3])}`,
+                    atcCode: `${record[4]}`,
+                    tinWithBranchCode: `${formatTIN(record[6])}`,
+                    sequenceNumber: `${record[7]}`,
+                    returnPeriod: `${record[8]}`,
+                }))
             break
         case 'sawt':
             details = records.slice(1, -1)
-            .map(record => ({
-                alphaListTypeCode: `D${alphaList.acronym},D${route.form_type}`,
-                sequenceNumber: `${record[7]}`,
-                tinWithBranchCode: `${formatTIN(record[0])}`,
-                corporation: `${formatCorpName(record[4])}`,
-                returnPeriod: `${formatDate(records[0][1])}`,
-                atcCode: `${record[1]}`,
-                taxRate: `${formatDigit(record[3])}`,
-                incomePayment: `${formatDigit(record[6])}`,
-                taxWithHeld: `${formatDigit(record[5])}`,
+                .map(record => ({
+                    alphaListTypeCode: `D${alphaList.acronym},D${route.form_type}`,
+                    sequenceNumber: `${record[7]}`,
+                    tinWithBranchCode: `${formatTIN(record[0])}`,
+                    corporation: `${formatCorpName(record[4])}`,
+                    returnPeriod: `${formatDate(records[0][1])}`,
+                    atcCode: `${record[1]}`,
+                    taxRate: `${formatDigit(record[3])}`,
+                    incomePayment: `${formatDigit(record[6])}`,
+                    taxWithHeld: `${formatDigit(record[5])}`,
+                }))
+            break
+        case 'st':
+            details = records.slice(1, -1).map(record => ({
+                alphaListTypeCode: `D,S`,
+                taxableMonth: `${record[10]}`,
+                tinWithBranchCode: `${formatTIN(record[7])}`,
+                registeredName: `${record[6]}`,
+                customerAddress: `${record[8]}`,
+                amtOfGrossSales: `${formatDigit(record[5])}`,
+                amtOfExemptSales: `${formatDigit(record[4])}`,
+                amtOfZeroRatedSales: `${formatDigit(record[3])}`,
+                amtOfTaxableSales: `${formatDigit(record[2])}`,
+                amtOfSalesOfServices: `${formatDigit(record[1])}`,
+                amtOfSalesOfGoods: `${formatDigit(record[0])}`,
+                amtOfOutputTax: `${formatDigit(record[8])}`,
+                amtOfGrossTaxableSales: `${formatDigit(record[11])}`
             }))
             break
     }
@@ -100,22 +132,22 @@ export function details(records, route, count) {
     return details
 }
 
-export function controls(record, route, count) {
+export function controls(record, route, count = 0) {
 
     const alphaList = reportTypes.find(reportType => reportType.id === route.report_type)
     let controlsData = {}
 
     switch (route.report_type) {
         case 'qap':
-            
+
             let returnPeriodYear = ''
 
             const months = record.slice(1, -1)
-            .map(record => {
-                const [month, day, year] = record[8].split('/');
-                returnPeriodYear = `${year}`
-                return parseInt(month);
-            });
+                .map(record => {
+                    const [month, day, year] = record[8].split('/');
+                    returnPeriodYear = `${year}`
+                    return parseInt(month);
+                });
             const smallestMonth = Math.min(...months) + count
 
             controlsData = {
@@ -125,7 +157,20 @@ export function controls(record, route, count) {
                 incomePayment: `${formatDigit(record[record.length - 1][4])}`,
                 taxWithHeld: `${formatDigit(record[record.length - 1][5])}`,
             }
-        break
+            break
+        case 'st':
+            controlsData = {
+                alphaListTypeCode: `D,S`,
+                amtOfGrossSales: `${formatDigit(record[record.length - 1][0])}`,
+                amtOfExemptSales: `${formatDigit(record[record.length - 1][1])}`,
+                amtOfZeroRatedSales: `${formatDigit(record[record.length - 1][2])}`,
+                amtOfTaxableSales: `${formatDigit(record[record.length - 1][3])}`,
+                amtOfSalesServices: `${formatDigit(record[record.length - 1][4])}`,
+                amtOfSalesOfGoods: `${formatDigit(record[record.length - 1][5])}`,
+                amtOfOutputTax: `${formatDigit(record[record.length - 1][6])}`,
+                amtOfTaxableSalesq: `${formatDigit(record[record.length - 1][8])}`,
+            }
+            break
         default:
             controlsData = {
                 alphaListTypeCode: `C${alphaList.acronym},C${route.form_type}`,
@@ -134,25 +179,25 @@ export function controls(record, route, count) {
                 incomePayment: `${formatDigit(record[record.length - 1][4])}`,
                 taxWithHeld: `${formatDigit(record[record.length - 1][5])}`,
             }
-        break
+            break
     }
     return controlsData
 }
 
-export function filename(record, route, count) {
+export function filename(record, route, count = 0) {
     let fileNameData = {}
 
     switch (route.report_type) {
         case 'qap':
-            
+
             let returnPeriodYear = ''
 
             const months = record.slice(1, -1)
-            .map(record => {
-                const [month, day, year] = record[8].split('/');
-                returnPeriodYear = `${year}`
-                return parseInt(month);
-            });
+                .map(record => {
+                    const [month, day, year] = record[8].split('/');
+                    returnPeriodYear = `${year}`
+                    return parseInt(month);
+                });
             const smallestMonth = Math.min(...months) + count
 
             fileNameData = {
@@ -161,7 +206,16 @@ export function filename(record, route, count) {
                 routeFormType: `${route.form_type}`,
                 extension: '.dat'
             }
-        break
+            break
+        case 'st':
+            console.log(record)
+            const [month, day, year] = record[1][10].split('/')
+            fileNameData = {
+                tinWithBranchCode: `${formatTIN(record[0][0]).replace(",", "")}S0`,
+                returnPeriod: `${month}${year}`,
+                extension: '.dat'
+            }
+            break
         default:
             fileNameData = {
                 tinWithBranchCode: `${formatTIN(record[0][0]).replace(",", "")}`,
@@ -169,7 +223,7 @@ export function filename(record, route, count) {
                 routeFormType: `${route.form_type}`,
                 extension: '.dat'
             }
-        break
+            break
     }
     return fileNameData
 }
