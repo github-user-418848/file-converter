47c336af6822d903a81df86176ecc5d2fcf34f55
github-user-418848 <x1cjlyjr@duck.com>
2023-06-25 14:09:13 +0800

    created BtnUploadFile component

---
+++ src/components/BtnUploadFile.vue
@@ -0,0 +1,21 @@
+<template>
+    <button class="btn col-md-3 col-8 mb-1" @click="uploadFile">Select File</button>
+    <input type="file" name="uploadFile" id="uploadFile" class="uploadFile" accept=".xml" @change="handleUploadFile"/>
+</template>
+
+<script>
+export default {
+    name: "BtnUploadFile",
+    emits: ['upload-file'],
+    methods: {
+        uploadFile() {
+            const dropzoneFileInput = document.querySelector('.uploadFile');
+            dropzoneFileInput.click();
+        },
+        handleUploadFile(e) {
+            const file = e.target.files[0];
+            this.$emit('upload-file', file);
+        }
+    }
+}
+</script>
