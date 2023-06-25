3c88e37ed100e5fa14f7353c1d9a0495b51285b4
github-user-418848 <x1cjlyjr@duck.com>
2023-06-26 00:50:43 +0800

    fixed SVG size

--- src/components/Toast.vue
+++ src/components/Toast.vue
@@ -2,7 +2,7 @@
     <transition name="toast">
         <div class="toast-wrapper" v-if="errorMessage">
             <div class="toast">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="37" height="37" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                     <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
                 </svg>
@@ -34,6 +34,7 @@ export default {
 	margin: 0 auto;
 	left: calc(clamp(0rem, 0rem + 100vw, 120rem) / 2 - var(--toast-width) / 2);
 }
+
 .toast {
     padding: clamp(0.625rem, 0.625rem + 0.5208vw, 1.25rem);
     color: #f84026;
@@ -45,29 +46,25 @@ export default {
     margin: 0 auto;
     display: inline;
 }
+
 .toast > .bi {
     vertical-align: middle;
 }
+
 .toast-enter-from {
     opacity: 0;
     transform: translateY(-120px)
 }
-/* .toast-enter-to {
-    opacity: 1;
-    transform: translateY(0px)
-} */
+
 .toast-enter-active {
     transition: all 0.3s ease;
 }
 
-/* .toast-leave-from {
-    opacity: 0;
-    transform: translateY(0px)
-} */
 .toast-leave-to {
     opacity: 1;
     transform: translateY(-120px)
 }
+
 .toast-leave-active {
     transition: all 0.3s ease;
 }
