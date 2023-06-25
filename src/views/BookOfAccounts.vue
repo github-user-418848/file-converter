b71278cd948ce4f166ba468f44f84b257efc207a
github-user-418848 <x1cjlyjr@duck.com>
2023-06-26 00:35:19 +0800

    revamp file conversion UI

--- src/views/BookOfAccounts.vue
+++ src/views/BookOfAccounts.vue
@@ -1,49 +1,51 @@
 <template>
-    <Toast :errorMessage="errorMessage" />
+    <div class="dropzone" @dragenter.prevent="toggleActive" @dragleave.prevent="toggleActive" @dragover.prevent
+        @drop.prevent="dropFile" :class="{ 'active-dropzone': active }">
+        <Toast :errorMessage="errorMessage" />
 
-    <h1>File Converter</h1>
-    <p>Convert financial reports to various file formats</p>
-    
-    <!-- Display records array -->
-    <div v-for="(record, index) in records" :key="index">
-        <p>{{ index }} - {{ record }}</p>
+        <h1>File Converter</h1>
+        <p>Convert financial reports to various file formats. Drag and Drop files are supported.</p>
+
+        <!-- Display records array -->
+        <!-- <div v-for="(record, index) in records" :key="index">
+            <p>{{ index }} - {{ record }}</p>
+        </div> -->
+
+        <BtnUploadFile @upload-file="uploadFile" />
+
+        <FormattingOptions />
+
+        <template v-for="(file, index) in dataFiles.originalFileName" :key="index">
+            <DownloadCard :fileName="file" :textData="dataFiles.textContent[index]"
+                :generatedFileName="dataFiles.generatedFileName[index]" />
+        </template>
     </div>
-
-    <!-- DropZone component to select XML file -->
-    <DropZone @drop.prevent="dropFile" @change="selectedFile" />
-
-    <!-- FormattingOptions component for formatting of the DAT output -->
-    <FormattingOptions />
-
-    <!-- DownloadCard component to download transformed file -->
-
-    <template v-for="(file, index) in dataFiles.originalFileName" :key="index">
-        <DownloadCard :fileName="file" :textData="dataFiles.textContent[index]"
-            :generatedFileName="dataFiles.generatedFileName[index]" />
-    </template>
 </template>
 
 <script>
-import DropZone from '../components/DropZone.vue'
+import BtnUploadFile from '../components/BtnUploadFile.vue'
 import DownloadCard from '../components/DownloadCard.vue'
 import FormattingOptions from '../components/FormattingOptions.vue'
 import Toast from '../components/Toast.vue'
 
-import { readXMLFile, retrieveRecords } from '../utils/helpers.js'
+
+import { createFormattedOutput } from '../utils/boaStructure.js'
+import { readXMLFile, retrieveRecords, createTextData } from '../utils/helpers.js'
 import { files } from '../utils/globals.js'
 import { validateXmlFile } from '../utils/validators.js'
 import { ref } from 'vue'
 
 export default {
-    name: "BookOfAccounts",
+    name: "ValueAddedTax",
     components: {
-        DropZone,
+        BtnUploadFile,
         DownloadCard,
         FormattingOptions,
         Toast,
     },
     data() {
         return {
+            active: ref(false),
             file: ref(""),
             records: ref(""),
             textData: ref(""),
@@ -53,14 +55,53 @@ export default {
         }
     },
     methods: {
+        async toggleActive() {
+            this.active = !this.active
+        },
+
         async dropFile(e) {
+            this.active = false
             this.file = e.dataTransfer.files[0]
             await this.parseXmlFile(this.file)
             console.log(this.file.name)
         },
 
-        async selectedFile() {
-            this.file = document.querySelector('.dropzoneFile').files[0]
+        async uploadFile() {
+
+            const record = [
+                ["MBTC/PDC-01.01.2023", "3126", "Security Agency & Intelligent Network Technology", "01/01/2023", "21533"],
+                ["1-11001", "MBTC-CA#392-7-39253433-0", "0.00", "62,500.00"],
+                ["1-12100", "Trade Debtors/ Trade Receivables", "62,500.00", "0.00"],
+                ["62,500.00", "62,500.00", "TOTAL"],
+                [],
+                ["LBP; PDC-01.05.2023/ FULL PAYMENT/ PART OF PHP 198,370.99", "4258", "RDF Feed, Livestock & Foods, Inc", "01/05/2023", "28171"],
+                ["1-11001", "MBTC-CA#392-7-39253433-0", "0.00", "112,924.56"],
+                ["1-12100", "Trade Debtors/ Trade Receivables", "112,924.56", "0.00"],
+                ["112,924.56", "112,924.56", "TOTAL"],
+                [],
+                ["LBP; BI-P0059/PART OF PHP 198,370.99/ PDC 01.05.2023", "4259", "RDF Feed, Livestock & Foods, Inc", "01/05/2023", "28173"],
+                ["1-11001", "MBTC-CA#392-7-39253433-0", "0.00", "85,446.43"],
+                ["1-17800", "Deferred Output VAT", "0.00", "9,154.98"],
+                ["1-12100", "Trade Debtors/ Trade Receivables", "85,446.43", "0.00"],
+                ["2-13300", "Output VAT Payable", "9,154.98", "0.00"],
+                ["94,601.41", "94,601.41", "TOTAL"],
+                []
+            ];
+
+            const formattedOutput = createFormattedOutput(record);
+
+            formattedOutput.forEach((data, index) => {
+                // console.log(`Record ${index + 1}:`);
+                if (data !== undefined) {
+
+                    Object.entries(data).forEach(([key, value]) => {
+                        console.log(`${value}`);
+                    });
+                }
+                // console.log('---');
+            });
+
+            this.file = document.querySelector('.uploadFile').files[0]
             await this.parseXmlFile(this.file)
             console.log(this.file.name)
         },
@@ -78,7 +119,7 @@ export default {
                 setTimeout(() => { this.errorMessage = "" }, 7000)
             }
         },
-        
+
         async createTextData(records) {
             const { params } = this.$route;
             const separator = ',';
