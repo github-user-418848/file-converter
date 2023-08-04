47be6811e93b2e806f331b21f97afbc6aa9e01bc
github-user-418848 <x1cjlyjr@duck.com>
2023-08-04 08:45:57 +0800

    removed comment

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -13,8 +13,6 @@ export function formatTIN(tin) {
 }
 
 export function formatDate(date) {
-    // console.log('date:', date);
-    // console.log('typeof date:', typeof date);
     const match = regexMatched(/MONTH\s+OF\s+([A-Za-z]*)\s+(\d*)/, date)
     if (match) {
         return ('0' + (new Date(match[2], getMonthIndex(match[1]), 1)).getMonth() + 1).slice(-2) + '/' + match[2]
@@ -25,8 +23,6 @@ export function formatDate(date) {
 }
 
 export function formatOwnersAddress(ownersAddress) {
-    // console.log('ownersAddress:', ownersAddress);
-    // console.log('typeof ownersAddress:', typeof ownersAddress);
     const match = regexMatched(/OWNER'S ADDRESS: (.*)/, ownersAddress)
     if (match) {
         return `"${match[1]}"`.replace(",", "")
@@ -37,8 +33,6 @@ export function formatOwnersAddress(ownersAddress) {
 }
 
 export function formatOwnersName(ownersName) {
-    // console.log('ownersName:', ownersName);
-    // console.log('typeof ownersName:', typeof ownersName);
     const match = regexMatched(/OWNER'S NAME: (.*)/, ownersName)
     if (match) {
         return `"${match[1]}"`
@@ -53,8 +47,6 @@ export function formatCorpName(corpName) {
 }
 
 export function formatAgentName(agentName) {
-    // console.log('agentName:', agentName);
-    // console.log('typeof agentName:', typeof agentName);
     const match = regexMatched(/WITHHOLDING AGENT'S NAME:\s*(.*)/, agentName)
     if (match) {
         return `"${match[1]}"`
@@ -69,9 +61,21 @@ export function formatDigit(digit) {
 }
 
 export const setTblHeaderFormat = (label, from, to, length, value) => {
-    return `${label.padEnd(15)}${from.toString().padEnd(5)}${to.toString().padEnd(6)}${length.toString().padEnd(7)}${value}\n`;
+    const columnWidth = 22;
+    const padding = ' '.repeat(columnWidth);
+
+    return `${label.padEnd(columnWidth)}${from.toString().padEnd(columnWidth)}${to.toString().padEnd(columnWidth)}${length.toString().padEnd(columnWidth)}${value}\n`;
 };
 
-export const setTblBodyFormat = (date, customer, referenceNo, journal, details, gLAccount, accountName, debit, credit) => {
+// export const setTblHeaderFormat = (label, from, to, length, value) => {
+//     return `${label.padEnd(15)}${from.toString().padEnd(5)}${to.toString().padEnd(6)}${length.toString().padEnd(7)}${value}\n`;
+// };
+
+export const setCashReceiptBookTblBodyFormat = (date, customer, referenceNo, journal, details, gLAccount, accountName, debit, credit) => {
     return `${date.padEnd(15)}${customer.padEnd(50)}${referenceNo.toString().padEnd(15)}${journal.toString().padEnd(19)}${details.padEnd(60)}${gLAccount.padEnd(20)}${accountName.padEnd(50)}${debit.toString().padEnd(14)}${credit.toString()}\n\n`;
+};
+
+
+export const setAuditTrailTblBodyFormat = (date, code, login, module, activity, refNum, postDate, details, amount) => {
+    return `${date.padEnd(25)}${code.padEnd(15)}${login.toString().padEnd(20)}${module.toString().padEnd(25)}${details.padEnd(75)}${refNum.padEnd(20)}${postDate.padEnd(50)}${details.toString().padEnd(14)}${amount.toString()}\n\n`;
 };
